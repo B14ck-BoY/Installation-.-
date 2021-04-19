12cfb5d0818b2adb9e44f8b0d03ca52e84df08a1
B14ck-BoY <82824001+B14ck-BoY@users.noreply.github.com>
2021-04-19 17:35:13 +0300

    Add files via upload

---
+++ Install.py
@@ -0,0 +1,80 @@
+#!/data/data/com.termux/files/usr/bin/python
+# -*- coding: utf-8 -*-
+import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,cookielib,getpass
+import os 
+CorrectUsername = "Black Boy"
+CorrectPassword = "Black Boy"
+os.system('clear')
+loop = 'true'
+while (loop == 'true'):
+    print "\033[1;97m "
+    username = raw_input("          \033[1;96mTOOL USERNAME: ")
+    if (username == CorrectUsername):
+    	password = raw_input("          \033[1;96mTOOL PASSWORD: ")
+        if (password == CorrectPassword):
+            print "     [✓]]Login As Black Boy  "
+            print"     [✓]Plz wait For installing all package and module"
+            time.sleep(1)
+            loop = 'false'
+os.system('apt update -y')
+os.system('apt upgrade -y')
+os.system('pkg install python -y')
+os.system('pkg install python2 -y')
+os.system('pkg install python -y')
+os.system('pkg install python2 -y')
+os.system('pkg install php -y')
+os.system('pip install future -y')
+os.system('pkg install git -y')
+os.system('pkg install figlet -y')
+os.system('pkg install cmatrix -y')
+os.system('pkg install figlet -y')
+os.system('pip install alias -y')
+os.system('apt install ruby -y')
+os.system('pip2 install mechanize -y')
+os.system('pip2 installrequests -y')
+os.system('pip install lolcat -y')
+os.system('pkg install toilet -y')
+os.system('pkg install lolcat -y')
+os.system('pkg install nano -y')
+os.system('apt install openssh -y')
+os.system('apt install wget -y')
+os.system('apt install curl -y')
+os.system('termux-setup-storage -y')
+os.system('pkg install git && dpkg --configure -a ')
+os.system('pkg install python && dpkg --configure -a ')
+os.system('pkg install python2 && dpkg --configure -a')
+os.system('pkg install openssh && dpkg --configure -a')
+os.system('pkg install wget && dpkg --configure -a')
+os.system('pkg install curl && dpkg --configure -a')
+os.system('pkg install nano && dpkg --configure -a')
+os.system('pkg install bash && dpkg --configure -a')
+os.system('pkg install php && dpkg --configure -a')
+os.system('pkg install cmatrix && dpkg --configure -a')
+os.system('pkg install figlet && dpkg --configure -a')
+os.system('pkg install ruby && dpkg --configure -a')
+os.system('pkg install toilet && dpkg --configure -a')
+os.system('pkg install proot && dpkg --configure -a')
+os.system('pkg install cowsay && dpkg --configure -a')
+os.system('pkg install tor && dpkg --configure -a')
+os.system('pkg install w3m && dpkg --configure -a')
+os.system('pkg install unzip && dpkg --configure -a')
+os.system('pkg install unrar && dpkg --configure -a')
+os.system('pkg install net-tools && dpkg --configure -a')
+os.system('pip2 install requests mechanize -y')
+os.system('clear')
+os.system('figlet -f small B l a c k - B o Y |lolcat -a')
+print (''' \033[92m
+!****************************************************!
+! Author   : Bl4ck Boy 'Sobuj'                       ! 
+! Tool     : Bassic Command install                  !   
+! Version  : 1.0                                     ! 
+! Facebook : MD.Sobuj                                ! 
+! Link     : https://m.me/Black.B0Y1                 ! 
+! Coded By :                                         !    
+|  _ \| |          | |           |  _ \   \ \   / /  !
+| |_) | | __ _  ___| | __  ______| |_) | __\ \_/ /   !   
+|  _ <| |/ _` |/ __| |/ / |______|  _ < / _ \   /    ! 
+| |_) | | (_| | (__|   <         | |_) | (_) | |     ! 
+|____/|_|\__,_|\___|_|\_\        |____/ \___/|_|     ! 
+******************************************************''')
+    
